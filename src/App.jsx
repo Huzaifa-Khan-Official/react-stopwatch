bf76a2a1f0e6fd73e5b5253828caf96f479b95ea
Huzaifa Khan <m.huzaifakhan2004@gmail.com>
2023-12-28 14:39:50 +0500

    start working

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Stopwatch from './Components/Timer/Stopwatch';
+
+function App() {
+  return (
+    <div className="App">
+      <Stopwatch />
+    </div>
+  );
+}
+
+export default App;
