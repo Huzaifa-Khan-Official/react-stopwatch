19073094c094a807047a78617af982d2cd99c253
Huzaifa Khan <m.huzaifakhan2004@gmail.com>
2023-12-28 22:55:53 +0500

    backend done

--- src/Components/Timer/Stopwatch.jsx
+++ src/Components/Timer/Stopwatch.jsx
@@ -1,59 +1,42 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 
 const Stopwatch = () => {
-  // const [isRunning, setIsRunning] = useState(false);
-  // const [elapsedTime, setElapsedTime] = useState(0);
+  const [second, setSecond] = useState(0);
+  const [minute, setMinute] = useState(0);
+  const [hour, setHour] = useState(0);
+  const [intervalId, setIntervalId] = useState(null);
+  
+  const startTimer = () => {
+    if (!intervalId) {
+      const id = setInterval(() => {
+        setSecond((prevSecond) => prevSecond + 1);
+      }, 1000);
+      setIntervalId(id);
+    }
+  };
 
-  // useEffect(() => {
-  //   let interval;
+  const stopTimer = () => {
+    clearInterval(intervalId);
+    setIntervalId(null);
+  };
 
-  //   if (isRunning) {
-  //     interval = setInterval(() => {
-  //       setElapsedTime(prevTime => prevTime + 1);
-  //     }, 1000);
-  //   } else {
-  //     clearInterval(interval);
-  //   }
+  const reset = () => {
+    stopTimer();
+    setSecond(0);
+    setMinute(0);
+    setHour(0);
+  };
 
-  //   return () => clearInterval(interval);
-  // }, [isRunning]);
-
-  // const startStop = () => {
-  //   setIsRunning(!isRunning);
-  // };
-
-  // const reset = () => {
-  //   setIsRunning(false);
-  //   setElapsedTime(0);
-  // };
-
-  // const formatTime = (timeInSeconds) => {
-  //   const minutes = Math.floor(timeInSeconds / 60);
-  //   const seconds = timeInSeconds % 60;
-  //   return `${String(minutes).padStart(2, '0')}:${String(seconds).padStart(2, '0')}`;
-  // };
-
-  const [millisec, setMillisec] = useState(0);
-  let milliseond = 0;
-  let second = 0;
-  let minute = 0;
-  let hours = 0;
-  let interval;
-
-  const start = () => {
-    interval = setInterval(() => {
-      milliseond++
-
-    })
-  }
-  start()
   return (
     <div>
-      <p id='millisecond'>{milliseond}</p>
-      {/* <h1>Stopwatch</h1>
-      <p>{formatTime(elapsedTime)}</p>
-      <button onClick={startStop}>{isRunning ? 'Stop' : 'Start'}</button>
-      <button onClick={reset}>Reset</button> */}
+      <p id='timer'>{hour} : {minute} : {second}</p>
+      <button onClick={startTimer} disabled={!!intervalId}>
+        Start
+      </button>
+      <button onClick={stopTimer} disabled={!intervalId}>
+        Stop
+      </button>
+      <button onClick={reset}>Reset</button>
     </div>
   );
 };
